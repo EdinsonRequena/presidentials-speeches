2d09be7f71c95da9a4fc7a5320430f33ea8c3aa4
Edinson Requena <edinsonrequena@protonmail.com>
2023-08-28 16:19:59 -0400

    feat: create signup screen && home screen with mocks

--- speeches/views.py
+++ speeches/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
 
 
 def signup(request):
-    return render(request, 'signup.html', {'form': UserCreationForm()})
+    form = UserCreationForm
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+
+            try:
+                # form.save()
+                user = User.objects.create_user(
+                    username=form.cleaned_data['username'],
+                    password=form.cleaned_data['password1'],
+                )
+                user.save()
+                login(request, user)
+                return redirect('home')
+
+            except Exception as e:
+                print(e)
+
+    return render(request, 'signup.html', {'form': form})
