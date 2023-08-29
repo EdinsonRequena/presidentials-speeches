12c6bb090d0c52f7493aa7fe25b3f6cb5a2d665f
Edinson Requena <edinsonrequena@protonmail.com>
2023-08-29 01:35:31 -0400

    feat: create login screen and functionality

--- speeches/views.py
+++ speeches/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 
 
 def home(request):
@@ -35,3 +35,19 @@ def signup(request):
 def singout(request):
     logout(request)
     return redirect('/')
+
+
+def signin(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/home')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})
