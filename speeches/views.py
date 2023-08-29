22dc26c675e160a40580e796592c76f0aa4e5f26
Edinson Requena <edinsonrequena@protonmail.com>
2023-08-29 02:06:07 -0400

    feat: protecting home route

--- speeches/views.py
+++ speeches/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='/login/')
 def home(request):
     return render(request, 'home.html')
 
@@ -17,7 +19,6 @@ def signup(request):
         if form.is_valid():
 
             try:
-                # form.save()
                 user = User.objects.create_user(
                     username=form.cleaned_data['username'],
                     password=form.cleaned_data['password1'],
